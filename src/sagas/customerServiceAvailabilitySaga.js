c01b7f7c27102d47e6d9d29cbdd7a4125dc369ae
Milad Vafaeifard <milad_vafaeifard@epam.com>
2018-07-16 14:48:50 +0200

    Implement eventChannel in the Application to handle service availability using socket and redux-saga

---
+++ src/sagas/customerServiceAvailabilitySaga.js
@@ -0,0 +1,23 @@
+import { put, take } from 'redux-saga/effects'
+import { eventChannel } from 'redux-saga';
+
+import { connect } from '../createSocketConnection';
+import { setCustomerServiceAvailability } from '../actions';
+
+export function* CustomerServiceAvailabilitySaga() {
+    const socket = connect();
+    const chan = eventChannel( emit => {
+        const enableSupportMessage = () => { emit(true); }
+        const disableSupportMessage = () => { emit(false); }
+
+        socket.on('SUPPORT_AVAILABLE', enableSupportMessage);
+        socket.on('SUPPORT_NOT_AVAILABLE', disableSupportMessage);
+
+        return () => {};
+    });
+
+    while(true) {
+        let supportAvailable = yield take(chan);
+        yield put(setCustomerServiceAvailability(supportAvailable));
+    }
+}
